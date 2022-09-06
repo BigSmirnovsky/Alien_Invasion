0741eaeb6cb2bd8d5384b57c7116d213b94debb8
siarheismirnou <smirnovsergei1377@gmail.com>
2022-09-06 16:24:45 +0300

    add moving left ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,9 +35,15 @@ class AlienInvasion:
                 if event.key == pygame.K_RIGHT:
                     # Переместить корабль вправо.
                     self.ship.moving_right = True
+                    # Переместить корабль влево.
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False    
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран"""
