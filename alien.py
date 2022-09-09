a570e4e451af106568de76027e2d2c5c22afceee
siarheismirnou <smirnovsergei1377@gmail.com>
2022-09-09 22:28:23 +0300

    add alien class

---
+++ alien.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Alien(Sprite):
+    """Классб представляющий одного пришельца"""
+    
+    def __init__(self, ai_game):
+        """Инициализирует пришельца и задает его начальную позицию."""
+        super().__init__()
+        self.screen = ai_game.screen
+
+        #Закргузка изображения и назначение атрибута rect
+        self.image = pygame.load('inages/alien_ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # Каждый новый пришелец появляется в левом верхнем углу экрана
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # Сохранение точной горизотальной позиции пришельца
+        self.x = float(self.rect.x)
