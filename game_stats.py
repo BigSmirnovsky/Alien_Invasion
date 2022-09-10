04de5a83115e1ce572a370908f493d917b290e86
siarheismirnou <smirnovsergei1377@gmail.com>
2022-09-11 00:09:38 +0300

    add game_stats.py

---
+++ game_stats.py
@@ -0,0 +1,12 @@
+class GameStats():
+    """Отслеживает статистику для игры Alien Invasion."""
+
+    def __init__(self, ai_game):
+        """Инициализирует статистику."""
+        self.settings = ai_game.settings
+        self.reset_stats()
+
+    def reset_stats(self):
+        """Инициализирует статистику, изменяющуюся в ходу игры."""
+        self.ship_left = self.settings.ship_limit
+        
