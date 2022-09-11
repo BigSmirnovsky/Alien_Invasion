a53dc20f0155f00c6990e0348abefa6ead079275
siarheismirnou <smirnovsergei1377@gmail.com>
2022-09-11 18:04:03 +0300

    add check_play_button function

--- alien_invasion.py
+++ alien_invasion.py
@@ -118,6 +118,11 @@ class AlienInvasion:
                 mouse_pos = pygame.mouse.get_pos()
                 self._chck_play_button(mouse_pos)
 
+    def _check_play_button(self, mouse_pos):
+        """Запускает новую игру при нажатии кнопки Play"""
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.stats.game_active = True
+
     def _check_keydown_events(self, event):
         """Реагирует на нажатие клавиш."""
         if event.key == pygame.K_RIGHT:
