423d5d925204626fd1dc6693ae971ed4f093230b
siarheismirnou <smirnovsergei1377@gmail.com>
2022-09-20 20:25:43 +0300

    change scorevoard add prep_score

--- scoreboard.py
+++ scoreboard.py
@@ -14,3 +14,14 @@ class Scoreboard():
         self.font = pygame.font.SysFont(None, 48)
         # подготовка исходного изображения.
         self.prep_score()
+
+    def prep_scrore(self):
+        """Преобразует текущий счёт в графическое изображение."""
+        score_str = str(self.stats.score)
+        self.score_image = self.font.render(score_str, True, 
+                self.text_color, self.settings.bg_color)
+
+        # Вывод счёта в правой верхней части экрана.
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20
+        self.score_rect.top = 20
