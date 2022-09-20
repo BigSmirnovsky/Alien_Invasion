da541897a1620c2023c314e93e0d538db222e881
siarheismirnou <smirnovsergei1377@gmail.com>
2022-09-20 19:52:54 +0300

    change settings

--- settings.py
+++ settings.py
@@ -2,14 +2,13 @@ class Settings():
     """Класс для хранения всех настроек игры Alien Invasion"""
 
     def __init__(self):
-        """Инициализирует настройки игры."""
+        """Инициализирует статические настройки игры."""
         # Параметры экрана
         self.screen_width = 900 
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
 
         # Настройки корабля
-        self.ship_speed = 1.5
         self.ship_limit = 3
 
         # Параметры снаряда
@@ -20,7 +19,14 @@ class Settings():
         self.bullet_allowed = 3
 
         # Настройки пришельцев
-        self.alien_speed = 1.0
+        #self.alien_speed = 1.0
         self.fleet_drop_speed = 10
         # fleet_direction = 1 обозначает движение вправо; а -1 влево.
-        self.fleet_direction = 1
+        #self.fleet_direction = 1
+
+        # Темп ускорения игры
+        self.speed_scale = 1.1
+
+        self.initialize_dunamyc_settings()
+
+        
